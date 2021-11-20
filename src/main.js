34dc951670e13308c4b714f8fcbce5512fac5fff
lucassouza <lucas.souza.rp1997@gmail.com>
2021-11-20 19:40:18 -0300

    Remove bootstrap-vue and replace for bootstrap 5.1.3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
-import './plugins/bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './plugins/axios'
 import App from './App.vue'
 import store from './store'
